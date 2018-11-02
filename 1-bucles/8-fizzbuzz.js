be6b1f126d78a3496f32df78041f10bba3fa7e0c
aginert <ana.ginert@gmail.com>
2018-11-02 20:23:43 +0100

    resuelto con else if + operador ternario, completo y funciona

--- 1-bucles/8-fizzbuzz.js
+++ 1-bucles/8-fizzbuzz.js
@@ -4,6 +4,24 @@
 // y los múltiplos de 5 imprima HUBS.
 // Además los múltiplos de 3 y 5 ha de imprimir GEEKSHUBS
 
+// function geeksHubs() {
+//     for ( var i = 1; i <= 100; i++) {
+//         if (i % 3 == 0 && i % 5 == 0) {
+//             console.log("GEEKSHUBS");
+
+//         }else if (i % 5 == 0) {
+//             console.log("HUBS");
+
+//         } else if (i % 3 == 0) {
+//             console.log("GEEKS")
+
+//         } else {
+//             console.log(i);
+//         }
+//     }
+// }
+
+// geeksHubs();
 
 for(var i = 1; i <= 100; i++){
     var e = i%3 & i%5 == 0;
