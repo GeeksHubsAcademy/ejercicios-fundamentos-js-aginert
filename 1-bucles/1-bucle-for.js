ba54888bacbfc32e616d9ac3298e4a962aa4eb51
aginert <ana.ginert@gmail.com>
2019-01-07 13:15:25 +0100

    Resuelto

--- 1-bucles/1-bucle-for.js
+++ 1-bucles/1-bucle-for.js
@@ -1,6 +1,7 @@
 // ej 1
 // haz un bucle que muestre por consola los números del 4 al 9 inclusive
 // Utilizad el bucle for
-for(var i = 4; i < 10; i++){
+//Le damos a i el valor 4 mostraremos por consola, siempre y ciando i sea menor que 10, añadiendole 1.
+for (var i = 4; i < 10; i++) {
     console.log(i);
 }
